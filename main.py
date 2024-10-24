aeb98c22ef99738cc3181bf1687dcc3b594cf245
yiqing-17 <B11200054@yuntech.edu.tw>
2024-10-24 16:01:29 +0800

    Modify main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    description: str = None
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get('/hello')
-def read_hello():
-    return '<p>Hello World</p>'
+@app.post("/items/")
+def create_item(item: Item):
+    print(f"Received item: {item}")
+    return {"message": "Item received", "item": item}
